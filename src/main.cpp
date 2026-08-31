86e3c2c85220383cc9720d85fafe426bfa8b9087
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-08-31 20:57:43 -0300

    setup

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+
+#include "./command_line_utils.cpp"
+
+
+int main(int argc, const char* argv[])
+{
+  bool is_verbose = is_string_present_in_argv("--verbose", argc, argv);
+  
+  if (is_verbose)
+  {
+    std::cout << "Modo Verboso: Ativo" << std::endl;
+  }
+
+  return EXIT_SUCCESS;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cctype>
+
+#include "./trace.hpp"
+
+/**
+ * @brief Separa a string provida em várias partes (explode) usando o delimitador
+ * como critério de separação
+ * 
+ * @param value 
+ * @param delimiter 
+ * @return std::vector<std::string> 
+ */
+std::vector<std::string> split_by(std::string value, char delimiter)
+{
+  std::vector<std::string> list;
+
+  size_t scanner_index = 0;
+  size_t current_word_start_index = 0;
+  
+  for (; scanner_index < value.length(); scanner_index++)
+  {
+    if (value.at(scanner_index) == delimiter)
+    {
+      list.push_back(value.substr(current_word_start_index, scanner_index - current_word_start_index));
+      current_word_start_index = scanner_index + 1;
+    }
+  }
+
+  list.push_back(value.substr(current_word_start_index, scanner_index - current_word_start_index));
+
+  return list;
+}
+
+bool contains(std::string text, char needle)
+{
+  for (size_t i = 0; i < text.length(); i++)
+  {
+    if (text.at(i) == needle)
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool contains(std::vector<std::string> &list, std::string &neddle_value)
+{
+  return std::find(list.begin(), list.end(), neddle_value) != list.end();
+}
+
+int64_t index_of(std::vector<std::string> &list, std::string &neddle_value)
+{
+  for (size_t i = 0; i < list.size(); i++)
+  {
+    if (list.at(i) == neddle_value)
+    {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+bool ignore_case_equals(const std::string &a, const std::string &b)
+{
+  auto char_equals = [](char a, char b) {
+    return std::tolower(a) == std::tolower(b);
+  };
+
+  return std::equal(a.begin(), a.end(), b.begin(), b.end(), char_equals);
+}
+
+std::string replace_char_with(const std::string &original_text, char character, const std::string &replacement)
+{
+  std::string new_string;
+  auto number_of_bytes = replacement.size();
+
+  size_t count = 0;
+  for (auto c : original_text)
+  {
+    count += (c == character) ? number_of_bytes : 1;
+  }
+
+  new_string.reserve(count);
+
+  for (auto c : original_text)
+  {
+    if (c == character) new_string += replacement;
+    else                new_string += c;
+  }
+
+  return new_string;
+}
+
+/**
+ * @brief retorna o símbolo representado como expressão
+ * @todo João, precisa ser validado se deve considerar outros símbolos além do "espaço"
+ * 
+ * @param symbol_name 
+ * @return std::string 
+ */
+std::string symbol_as_expression(const std::string &symbol_name)
+{
+  if (contains(symbol_name, ' '))
+  {
+    return "\"" + symbol_name + "\"";
+  }
+
+  return symbol_name;
+}
