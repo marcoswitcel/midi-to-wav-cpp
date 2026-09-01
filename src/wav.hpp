677b69b20a406a79eeab8a20400a5dcc3befcdfc
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-08-31 22:03:38 -0300

    WIP: header

--- src/wav.hpp
+++ src/wav.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
 
-struct Wav_File
+#pragma pack(push, 1)
+/**
+ * @brief 
+ * @url https://en.wikipedia.org/wiki/WAV#WAV_file_header
+ */
+struct WAV_File_Header
 {
+  // Master RIFF chunk
+  char riff[4] = {'R', 'I', 'F', 'F'};
+  uint32_t chunkSize;
+  char wave[4] = {'W', 'A', 'V', 'E'};
+  // Chunk describing the data format
+  char fmt[4]  = {'f', 'm', 't', ' '};
   // @todo João, implementar
 };
+#pragma pack(pop)
