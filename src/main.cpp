497fbafa71146f6e5708311a642c8d2908a620ab
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-09-05 14:10:44 -0300

    WIP: wav file generation

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <unordered_map>
 
-
 #include "./command_line_utils.cpp"
 #include "./midi.hpp"
 #include "./wav.hpp"
@@ -17,6 +16,18 @@ int main(int argc, const char* argv[])
   {
     std::cout << "Modo Verboso: Ativo" << std::endl;
   }
+
+  WAV_File wav_file;
+  std::string filename = "teste1.wav";
+
+  uint32_t sample_rate_in_hertz = 44100;
+  float duration_in_Seconds = 2.0;
+  uint32_t data_size = duration_in_Seconds *  sample_rate_in_hertz;
+  uint8_t* data_sample = new uint8_t[data_size];
+
+  init_wav_file(wav_file, sample_rate_in_hertz, 1, data_sample, data_size);
+
+  export_wav(filename, wav_file);
 
   return EXIT_SUCCESS;
 }
--- src/wav.hpp
+++ src/wav.hpp
@@ -2,7 +2,7 @@
 
 
 #include <string>
-
+#include <fstream>
 
 enum Audio_Format
 {
@@ -38,6 +38,9 @@ struct WAV_File_Header
 };
 #pragma pack(pop)
 
+constexpr auto STANDARD_RIFF_WAV_SIZE_IN_BYTES = 44;
+static_assert(sizeof(WAV_File_Header) == STANDARD_RIFF_WAV_SIZE_IN_BYTES, "Deve ter 44 bytes"); 
+
 /**
  * @brief Representa o arquivo na integra
  * 
@@ -45,18 +48,56 @@ struct WAV_File_Header
 struct WAV_File
 {
   WAV_File_Header header;
-  uint8_t *data;
+  uint8_t *data_sample;
 };
 
-void init_wav(WAV_File_Header &wav_file_header, uint32_t sample_rate, uint16_t num_channels)
+/**
+ * @brief realiza o processo de update dos campos apropriados para definir o tamanho do arquivo que deve ser escrito
+ * 
+ * @param wav_file_header 
+ * @param data_size 
+ */
+inline void patch_wav_file_header_size(WAV_File_Header &wav_file_header, uint32_t data_size)
 {
-  wav_file_header.sample_rate = sample_rate;
-  wav_file_header.num_channels = num_channels;
-  // @todo João, concluir aqui...
+  // ajusta tamanho do arquivo
+  wav_file_header.file_size = (STANDARD_RIFF_WAV_SIZE_IN_BYTES - 8) + data_size; 
+  // define quantidade de samples (em bytes)
+  wav_file_header.data_size = data_size;
 }
 
-bool export_wav(const std::string &filename, WAV_File_Header &wav_file_header, const std::vector<uint16_t> &data)
+void init_wav_file_header(WAV_File_Header &wav_file_header, uint32_t sample_rate, uint16_t num_channels, uint32_t data_size)
 {
-  // @todo João, concluir....
-  return false;
+  wav_file_header.bits_per_sample = 16;
+
+  wav_file_header.num_channels = num_channels;
+  wav_file_header.sample_rate = sample_rate;
+  
+  wav_file_header.bytes_per_block = (num_channels * wav_file_header.bits_per_sample / 8);
+  wav_file_header.byte_per_sec = sample_rate * wav_file_header.bytes_per_block;
+
+  patch_wav_file_header_size(wav_file_header, data_size);
+}
+
+void init_wav_file(WAV_File &wav_file, uint32_t sample_rate, uint16_t num_channels, uint8_t* data_sample, uint32_t data_size)
+{
+  init_wav_file_header(wav_file.header, sample_rate, num_channels, data_size);
+
+  wav_file.data_sample = data_sample;
+}
+
+bool export_wav(const std::string &filename, WAV_File &wav_file)
+{
+  std::ofstream file(filename, std::ios::binary);
+  
+  // @note por hora sem log do motivo do erro...
+  if (!file) return false;
+
+  file.write(reinterpret_cast<const char *>(&wav_file.header), sizeof(WAV_File_Header));
+  file.write(reinterpret_cast<const char *>(wav_file.data_sample), wav_file.header.data_size);
+
+  // força o fechamento
+  file.close();
+
+  // responde ocorreu tudo bem, inclusive com o fechamento
+  return !file.fail();
 }
